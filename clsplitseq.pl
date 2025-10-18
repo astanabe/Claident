9c16ac42f300b178366ce18b62872b683bae3cb6
astanabe <akifumi.tanabe@gmail.com>
2025-10-19 06:26:36 +0900

    BZIP2 compressed file handling problem of clsplitseq has been fixed.

--- clsplitseq.pl
+++ clsplitseq.pl
@@ -1911,7 +1911,7 @@ sub writeFile {
 		}
 	}
 	elsif ($filename =~ /\.bz2$/i) {
-		unless (open($filehandle, "| pbzip2 -p $qthreads -c >> $filename 2> $devnull")) {
+		unless (open($filehandle, "| pbzip2 -p$qthreads -c >> $filename 2> $devnull")) {
 			&errorMessage(__LINE__, "Cannot open \"$filename\".");
 		}
 	}
